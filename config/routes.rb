94fb8e46253e9c7d74f3b15ec377ca0e85bf4cc5
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-17 14:43:13 +0000

    Rakes passed, layout to be amended

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+# post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+# end
+
+
+# get 'restaurants', to: 'restaurants#index'
+# get 'restaurants/new', to: 'restaurants#new'
+# post 'restaurants', to: 'restaurants#create'
+# get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
